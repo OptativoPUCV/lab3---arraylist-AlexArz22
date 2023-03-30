01204ab8c0ec77485db6bbd033072fa8bc0a70c3
Replit user <>
2023-03-30 17:20:48 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -68,7 +68,11 @@ int get_size(ArrayList * l){
 
 //remove elements
 void clean(ArrayList * l){
+  for(int i=0; i<l->size; i++){
+    l->data[i]=NULL;
+  }
   l->size=0;
   l->capacity=2;
+  
   l->data=realloc(l->data,l->capacity*sizeof(void));
 }
